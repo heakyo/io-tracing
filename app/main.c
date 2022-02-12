93211859b7992c82bed1912294d79514365e3b1b
Marvin Ma <marvin.ma@dell.com>
2022-02-12 12:21:27 -0500

    read/write device directly
    Signed-off-by: Marvin Ma <marvin.ma@dell.com>

--- app/main.c
+++ app/main.c
@@ -8,15 +8,27 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define IOT_PRINT(fmt, ...)    \
+	do {                   \
+		if (v)         \
+			printf(fmt, ## __VA_ARGS__); \
+	} while(0)
+
 #define BUFSZ 4096
 
 void usage()
 {
 	printf("Usage:\n\t"
-		"main [options] [<device>]\n"
-		"Options:\n"
-		" -w, --write\t write the dev\n"
-		" -r, --read\t read the dev\n"
+		"main [OPTIONS] [<device>]\n"
+		"\nOptions:\n"
+		"[  --write, -w ] --- write data to the device\n"
+		"[  --read,  -r ] --- read data from the device\n"
+		"[  --pattern=<PTN>, -p <PTN> ]  --- data pattern\n"
+		"[  --verbose, -v ] --- print more information\n"
+		"[  --help, -h ] --- show help\n"
+		"\nExamples:\n"
+		"\t1.write data to the device dm-0 with pattern 0xb5\n"
+		"\tmain -p a5 -w /dev/dm-0\n"
 		);
 	_exit(-1);
 }
@@ -24,21 +36,27 @@ void usage()
 int main(int argc, char *argv[])
 {
 	char dev[32];
-	int wr, rd;
+	int wr, rd, v;
 	int fd, ret;
-	char rdbuf[BUFSZ];
-	char *aligned_wrbuf;
+	unsigned char wrdata_pattern;
+	unsigned char *wrbuf, *rdbuf;
 
 	char ch;
-	char *short_opts = "wr";
+	char *short_opts = "wrp:vh";
 	struct option long_opts[] = {
 		{"write", no_argument, NULL, 'w'},
 		{"read", no_argument, NULL, 'r'},
+		{"pattern", required_argument, NULL, 'p'},
+		{"verbose", no_argument, NULL, 'v'},
+		{"help", no_argument, NULL, 'h'},
 		{NULL, 0, NULL, 0},
 	};
 
 	wr = rd = 0;
+	v = 0;
 	opterr = 0;
+
+	wrdata_pattern = 0;
 
 	while ((ch = getopt_long_only(argc, argv, short_opts, long_opts, NULL)) != -1) {
 		switch (ch) {
@@ -47,6 +65,15 @@ int main(int argc, char *argv[])
 			break;
 		case 'r':
 			rd = 1;
+			break;
+		case 'p':
+			wrdata_pattern = strtol(optarg, NULL, 16);
+			break;
+		case 'v':
+			v = 1;
+			break;
+		case 'h':
+			usage();
 			break;
 		default:
 			printf("Unknown option: %c\n", ch);
@@ -59,11 +86,7 @@ int main(int argc, char *argv[])
 		usage();
 	}
 
-	ret = posix_memalign((void **)&aligned_wrbuf, BUFSZ, BUFSZ);
-	if (ret) {
-		printf("posix_memalign failed! %s\n", strerror(ret));
-		return -1;
-	}
+	IOT_PRINT("Start test.....\n");
 
 	strcpy(dev, argv[optind]);
 	fd = open(dev, O_RDWR | O_CREAT | O_SYNC | O_DIRECT, S_IRWXU | S_IRWXG | S_IRWXO);
@@ -72,28 +95,48 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	memset(rdbuf, 0x0, sizeof(rdbuf));
-	memset(aligned_wrbuf, 0xa5, BUFSZ);
-
 	if (wr) {
-		ret = write(fd, aligned_wrbuf, BUFSZ);
+		ret = posix_memalign((void **)&wrbuf, BUFSZ, BUFSZ);
+		if (ret) {
+			printf("posix_memalign failed! %s\n", strerror(ret));
+			return -1;
+		}
+		memset(wrbuf, wrdata_pattern, BUFSZ);
+		IOT_PRINT("wr:buf:%p pattern:0x%x\n", wrbuf, wrdata_pattern);
+
+		ret = write(fd, wrbuf, BUFSZ);
 		if (ret == -1) {
 			perror("write failed.");
 			return ret;
 		}
+
+		free(wrbuf);
 	}
 
+	lseek(fd, 0, SEEK_SET);
+
 	if (rd) {
-		ret = read(fd, rdbuf, sizeof(rdbuf));
+		ret = posix_memalign((void **)&rdbuf, BUFSZ, BUFSZ);
+		if (ret) {
+			printf("posix_memalign failed! %s\n", strerror(ret));
+			return -1;
+		}
+		memset(rdbuf, 0x0, BUFSZ);
+
+		ret = read(fd, rdbuf, BUFSZ);
 		if (ret == -1) {
 			perror("read failed.");
 			return ret;
 		}
+
+		IOT_PRINT("rd:buf:%p 0x%x--0x%x\n", rdbuf, rdbuf[0], rdbuf[BUFSZ-1]);
+
+		free(rdbuf);
 	}
 
 	close(fd);
 
-	free(aligned_wrbuf);
+	IOT_PRINT("End test.....\n");
 
 	return 0;
 }
