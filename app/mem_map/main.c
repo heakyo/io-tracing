64c4f6fe2826b710c1b85e6bbde68c26753b089f
Marvin <marvin.ma@dell.com>
2023-02-16 14:20:14 +0800

    Memory Category
    Signed-off-by: Marvin <marvin.ma@dell.com>

---
+++ app/mem_map/main.c
@@ -0,0 +1,18 @@
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <libaio.h>
+#include <assert.h>
+
+int main(int argc, char *argv[])
+{
+        getchar();
+        return 0;
+}
