bb0c9375c30255122c0bc75ce2cdd9d7165a3d6c
Marvin Ma <marvin.ma@dell.com>
2023-03-02 21:14:14 -0500

    elf test fist commit
    Signed-off-by: Marvin Ma <marvin.ma@dell.com>

---
+++ app/elf_test/main.c
@@ -0,0 +1,23 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <libaio.h>
+#include <assert.h>
+
+extern int subdata;
+extern int sub_func(int num);
+
+int main(int argc, char *argv[])
+{
+	int result = sub_func(subdata);
+
+        getchar();
+
+        return 0;
+}
---
+++ app/elf_test/sub.c
@@ -0,0 +1,6 @@
+int subdata = 100;
+
+int sub_func(int num)
+{
+	return num*num;
+}
