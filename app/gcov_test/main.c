27b871dbe33d5edb421d20733161d56c583cb13b
Marvin Ma <marvin.ma@dell.com>
2023-09-27 03:02:46 +0000

    add gcov test files
    Signed-off-by: Marvin Ma <marvin.ma@dell.com>

---
+++ app/gcov_test/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void print_hello()
+{
+	printf("Hello world\n");
+}
+
+void print_bye()
+{
+	printf("Goodbye world\n");
+}
+
+int main(int argc, char *argv[])
+{
+	print_hello();
+
+	return 0;
+}
