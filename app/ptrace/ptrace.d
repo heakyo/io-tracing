bc16eac2755d1630a4a609f6ef946361caec4290
Marvin Ma <marvin.ma@dell.com>
2023-10-06 09:21:41 +0000

    ptrace -- update files
    Signed-off-by: Marvin Ma <marvin.ma@dell.com>

--- app/ptrace/ptrace.d
+++ app/ptrace/ptrace.d
@@ -3,11 +3,16 @@
 /*
  * test command:
  * 	./ptrace.d -c './main'
+ * 	./ptrace.d -c './ptrace_attach_example pid'
  *
- * #define PT_TRACE_ME 0
- * #define PT_CONTINUE 7
- * #define PT_ATTACH 10
+ * #define PT_TRACE_ME  0
+ * #define PT_CONTINUE  7
+ * #define PT_ATTACH    10
+ * #define PT_DETACH    11
  *
+ * ./ptrace_attach_example:
+ *     1. run example first
+ *     2. ./ptrace_attach_example `pgrep example`
  */
 
 #pragma D option flowindent
@@ -16,14 +21,16 @@ BEGIN
 {
 	printf("-----IO Tracing Start-----");
 
-	proc = "main";
+	/*proc = "main";*/
+	proc = "ptrace_attach_examp";
+
+	/*g_pf_p = (struct proc *)0;*/
 }
 /*common*************************************************************************************/
 syscall::ptrace:entry,
 syscall::ptrace:return
 /execname == proc && 0/
 {}
-
 
 /*entry**************************************************************************************/
 sys_ptrace:entry
@@ -35,16 +42,21 @@ sys_ptrace:entry
 	printf("ptrace_args:req:%d",
 		self->uap->req
 		);
-
-	printf("\n\t\t\t\t\t      ");
 }
 
 kern_ptrace:entry
 /execname == proc/
 {
 	this->kp_td = args[0];
+	this->kp_req = args[1];
 
 	this->kp_p = this->kp_td->td_proc;
+
+	printf("req:%d",
+		this->kp_req
+		);
+
+	printf("\n\t\t\t\t\t      ");
 
 	printf("proc:%p: comm:%s flag:%p flag2:%p",
 		this->kp_p,
@@ -52,17 +64,86 @@ kern_ptrace:entry
 		this->kp_p->p_flag,
 		this->kp_p->p_flag2
 		);
+
+}
+
+pfind:entry
+/execname == proc/
+{
+	this->pf_pid = args[0];
+
+	printf("pid:%d",
+		this->pf_pid
+		);
 }
 
 proc_set_traced:entry
 /execname == proc/
-{}
+{
+	this->pst_p = args[0];
+	this->pst_stop = args[1];
+
+	printf("p:%p stop:%d",
+		this->pst_p,
+		this->pst_stop
+		);
+
+	printf("\n\t\t\t\t\t      ");
+	printf("proc: comm:%s flag:%p flag2:%p",
+		this->pst_p->p_comm,
+		this->pst_p->p_flag,
+		this->pst_p->p_flag2
+	);
+}
+
+kern_psignal:entry
+/execname == proc/
+{
+	this->kpe_p = args[0];
+	this->kpe_sig = args[1];
+
+	printf("p:%p sig:%d",
+		this->kpe_p,
+		this->kpe_sig
+	);
+}
 
 /*return*************************************************************************************/
 
-proc_set_traced:return
+kern_psignal:return
 /execname == proc/
 {}
+
+proc_set_traced:return
+/execname == proc/
+{
+	printf("proc: comm:%s flag:%p flag2:%p pid:%d",
+		this->pst_p->p_comm,
+		this->pst_p->p_flag,
+		this->pst_p->p_flag2,
+		this->pst_p->p_pid
+		);
+
+	printf("\n\t\t\t\t\t      ");
+	this->pst_pptr = this->pst_p->p_pptr;
+	printf("pp: comm:%s pid:%d",
+		this->pst_pptr->p_comm,
+		this->pst_pptr->p_pid
+		);
+}
+
+pfind:return
+/execname == proc/
+{
+	this->pf_p = (struct proc *)args[1];
+
+	printf("ret:proc:%p comm:%s flag:%p flag2:%p",
+		this->pf_p,
+		this->pf_p->p_comm,
+		this->pf_p->p_flag,
+		this->pf_p->p_flag2
+		);
+}
 
 kern_ptrace:return
 /execname == proc/
@@ -72,6 +153,12 @@ kern_ptrace:return
 		this->kp_p->p_comm,
 		this->kp_p->p_flag,
 		this->kp_p->p_flag2
+		);
+
+	printf("\n\t\t\t\t\t      ");
+	printf("proc:%p comm:%s",
+		this->pf_p,
+		this->pf_p->p_comm
 		);
 }
 
--- app/ptrace/ptrace_attach_example.c
+++ app/ptrace/ptrace_attach_example.c
@@ -7,37 +7,43 @@
 
 int main(int argc, char *argv[]) {
 
-    if (argc != 2) {
-        printf("Usage: %s <pid>\n", argv[0]);
-        exit(1);
-    }
+	pid_t target_pid = atoi(argv[1]);
+	int status;
 
-    pid_t target_pid = atoi(argv[1]);
-    int status;
+	if (argc != 2) {
+		printf("Usage: %s <pid>\n", argv[0]);
+		exit(1);
+	}
 
-    printf("Attaching to process %d...\n", target_pid);
-    if (ptrace(PT_ATTACH, target_pid, NULL, 0) != 0) {
-        perror("ptrace(PT_ATTACH)");
-        exit(1);
-    }
+	printf("Attaching to process %d...\n", target_pid);
+	getchar();
 
-    // Wait for the target process to stop
-    if (waitpid(target_pid, &status, 0) < 0) {
-        perror("waitpid");
-        exit(1);
-    }
+	if (ptrace(PT_ATTACH, target_pid, NULL, 0) != 0) {
+		perror("ptrace(PT_ATTACH)");
+		exit(1);
+	}
 
-    printf("Loop......\n");
-    while (1);
 
-    printf("Process %d stopped. Detaching...\n", target_pid);
+	printf("Wait for the target process to stop...");
+	getchar();
 
-    if (ptrace(PT_DETACH, target_pid, NULL, 0) != 0) {
-        perror("ptrace(PT_DETACH)");
-        exit(1);
-    }
+	// Wait for the target process to stop
+	if (waitpid(target_pid, &status, 0) < 0) {
+		perror("waitpid");
+		exit(1);
+	}
 
-    printf("Detached from process %d.\n", target_pid);
+	printf("Loop......\n");
+	getchar();
 
-    return 0;
+	printf("Process %d stopped. Detaching...\n", target_pid);
+
+	if (ptrace(PT_DETACH, target_pid, NULL, 0) != 0) {
+		perror("ptrace(PT_DETACH)");
+		exit(1);
+	}
+
+	printf("Detached from process %d.\n", target_pid);
+
+	return 0;
 }
