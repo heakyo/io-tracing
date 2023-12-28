57dfed16bb263a057e30a9d95236100f58eac0e5
Marvin Ma <marvin.ma@dell.com>
2023-12-28 07:27:10 +0000

    GCOV: add file README and code coverage in the Makefile
    Signed-off-by: Marvin Ma <marvin.ma@dell.com>

--- app/gcov_test/main.c
+++ app/gcov_test/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-void print_hello()
+void print_hello(void);
+void print_bye(void);
+
+void print_hello(void)
 {
 	printf("Hello world\n");
 }
 
-void print_bye()
+void print_bye(void)
 {
 	printf("Goodbye world\n");
 }
@@ -13,6 +16,7 @@ void print_bye()
 int main(int argc, char *argv[])
 {
 	print_hello();
+	print_bye();
 
 	return 0;
 }
