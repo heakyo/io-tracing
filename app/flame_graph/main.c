98bd5b685df46cac40f73db90bd08ed11bfbca6f
Marvin Ma <marvin.ma@dell.com>
2024-02-29 13:54:54 +0000

    APP: add flame graph
    Signed-off-by: Marvin Ma <marvin.ma@dell.com>

---
+++ app/flame_graph/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void caculate()
+{
+    for (int i = 0; i < 10000000; i++);
+}
+
+void funcE()
+{
+    caculate();
+}
+
+void funcD()
+{
+    funcE();
+}
+
+void funcA()
+{
+    funcD();
+}
+
+void funcB()
+{
+    caculate();
+}
+
+void funcC()
+{
+    caculate();
+}
+
+int main(int argc, char *args[])
+{
+    int i;
+
+    printf("Hello Flame Graph\n");
+
+    for (i = 0; i < 100; i++) {
+
+        if (i < 10) {
+            funcA();
+        } else if (i < 16) {
+            funcB();
+        } else {
+            funcC();
+        }
+    }
+
+    return 0;
+}
