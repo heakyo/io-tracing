b5676e73617faaf7fc710c82b734abd7732f66bb
Marvin Ma <marvin.ma@dell.com>
2025-01-22 07:01:36 -0500

    DOCKER: update startup.sh
    Signed-off-by: Marvin Ma <marvin.ma@dell.com>

--- docker/docker-dbg/test-controller/startup.sh
+++ docker/docker-dbg/test-controller/startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# B_MAIN_3995
 VERSION=$1
-DOCKER_VERSION=$(./artifactory-docker-image.py latest test-controller)
+DOCKER_NAME=$2
+
 DOCKER_IMAGE=pscale.artifactory.cec.lab.emc.com/pscale-docker-local/test-controller
 MYUSER=mam28
 MYTOKEN=cmVmdGtuOjAxOjE3NjA1OTM4MzE6OGJnd1VNOGhpTTZqSmFSTUJBUVhUWUNiR1dE
@@ -12,15 +12,19 @@ BIN_PATH=$(realpath ../)/bin
 usage()
 {
 	echo -e "Usage: \n\t$0 [VERSION]"
+
+	# B_MAIN_3995
 	echo -e "Example:\n\t$0 B_MAIN_3995"
 }
 
 run_test_controller()
 {
 
-	echo "##### Start test-controller: VERSION-${VERSION} #####"
+	echo "##### Start test-controller: VERSION-${VERSION} DOCKER NAME-${DOCKER_NAME} #####"
+
+	DOCKER_VERSION=$(./artifactory-docker-image.py latest test-controller)
 	docker pull ${DOCKER_IMAGE}:${DOCKER_VERSION}
-	docker run -d -it --rm --name=test-controller --network=host \
+	docker run -d -it --rm --name=$DOCKER_NAME --network=host \
 		-v $BIN_PATH:/root/bin \
 		-v /mnt/qalogserver_cn/qa:/mnt/qalogserver/qa \
 		-e BUILD_ID=${VERSION} \
@@ -38,9 +42,13 @@ main()
 		exit -1
 	fi
 
-	echo $BIN_PATH
-	#run_test_controller $1
+	if [ -z $2 ]; then
+		DOCKER_NAME=test-controller
+	fi
+
+	echo $BIN_PATH $DOCKER_NAME
+	run_test_controller
 }
 
 # Main Entry #
-main $VERSION
+main $VERSION $DOCKER_NAME
