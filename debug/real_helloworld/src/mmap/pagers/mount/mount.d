b84e154cbd7fc4ad902d4605b66f9be3e950895c
Marvin Ma <marvin.ma@dell.com>
2025-11-23 06:29:44 +0000

    MNT: debug ufs mount
    Signed-off-by: Marvin Ma <marvin.ma@dell.com>

---
+++ debug/real_helloworld/src/mmap/pagers/mount/mount.d
@@ -0,0 +1,112 @@
+#!/usr/sbin/dtrace -s
+
+/*
+ * test command:
+ * 	./mount.d -c 'mount /dev/md9 ufsimg'
+ */
+
+#pragma D option flowindent
+
+BEGIN
+{
+	procname = "mount";
+	ffsmnt = 0;
+
+	/* @[stack()] = count() */
+	printf("-----IO Tracing Start-----");
+}
+/*common*************************************************************************************/
+fdinit:*
+/execname == procname && ffsmnt/
+{}
+
+/*entry**************************************************************************************/
+sys_nmount:entry
+/execname == procname/
+{
+	this->td = args[0];
+
+	printf("td:td_proc->p_fd->fd_pwd.__ptr:%p",
+		this->td->td_proc->p_fd->fd_pwd.__ptr
+		);
+	printf("\n\t\t\t\t\t      ");
+
+	printf("td:%p curthread:%p", this->td, curthread);
+	printf("\n\t\t\t\t\t      ");
+
+	this->pwd = (struct pwd *)this->td->td_proc->p_fd->fd_pwd.__ptr;
+	printf("pwd:pwd_rdir:%p", this->pwd->pwd_rdir);
+}
+
+ffs_mount:entry
+/execname == procname/
+{
+	trace(probename);
+
+	stack();
+
+	ffsmnt = 1;
+}
+
+namei:entry
+/execname == procname && ffsmnt/
+{}
+
+namei_handle_root:entry
+/execname == procname && ffsmnt/
+{
+	this->ndp = args[0];
+
+	this->cnp = &this->ndp->ni_cnd;
+
+	printf("cnp:nameptr:%s", stringof(this->cnp->cn_nameptr));
+	printf("\n\t\t\t\t\t      ");
+
+	printf("ndp:rootdir:%p", this->ndp->ni_rootdir);
+	printf("\n\t\t\t\t\t      ");
+
+	printf("td:td_proc->p_fd->fd_pwd.__ptr:%p",
+		curthread->td_proc->p_fd->fd_pwd.__ptr
+		);
+	printf("\n\t\t\t\t\t      ");
+
+	this->pwd = (struct pwd *)curthread->td_proc->p_fd->fd_pwd.__ptr;
+	printf("pwd:pwd_rdir:%p", this->pwd->pwd_rdir);
+}
+
+mountcheckdirs:entry
+/execname == procname/
+{
+}
+
+/*return*************************************************************************************/
+mountcheckdirs:return
+/execname == procname/
+{}
+
+namei_handle_root:return
+/execname == procname && ffsmnt/
+{}
+
+namei:return
+/execname == procname && ffsmnt/
+{}
+
+ffs_mount:return
+/execname == procname/
+{
+	trace(probename);
+
+	ffsmnt = 0;
+}
+
+sys_nmount:return
+/execname == procname/
+{}
+
+/********************************************************************************************/
+END
+{
+	printf("-----IO Tracing END-----");
+	printf("\n\t\t\t\t\t      ");
+}
