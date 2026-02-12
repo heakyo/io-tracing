20d885e18fcac4d01d03e08042ddb175857a1253
Marvin Ma <marvin.ma@dell.com>
2026-02-12 09:09:02 +0000

    IOFLOW: rd ioflow complete
    Signed-off-by: Marvin Ma <marvin.ma@dell.com>

--- debug/real_helloworld/src/mmap/pagers/ioflow/main.c
+++ debug/real_helloworld/src/mmap/pagers/ioflow/main.c
@@ -1,32 +1,121 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <string.h>
 #include <assert.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 
 #define BUFSZ (8*1024)
 
-int main(int argc, char *argv[])
+bool run_pread, run_sysmap;
+
+static int
+read_file(void)
 {
+	unsigned char buf[BUFSZ], *p1, c;
 	int fd;
-	unsigned char buf[BUFSZ];
+	off_t offset;
 	int ret;
+        size_t length;
+        struct stat sb;
+
+	memset(buf, 0x0C, sizeof(buf));
 
 	fd = open("ufs2demo_mntdir/myfirstfile", O_CREAT | O_RDWR | O_SYNC, 0777);
-	assert(fd != -1);
+	if (fd == -1) {
+		perror("Failed to open file");
+		exit(-1);
+	}
 
-#if 0
-	memset(buf, 0xb5, sizeof(buf));
-	ret = write(fd, buf, sizeof(buf));
+	//ret = pwrite(fd, buf, sizeof(buf), 0);
+	ret = 0;
 	assert(ret != -1);
-#else
-	memset(buf, 0x0, sizeof(buf));
-	ret = read(fd, buf, sizeof(buf));
-	assert(ret != -1);
-#endif
 
-	//printf("ret:%d\n", ret);
+	if (run_sysmap) {
+
+		ret = fstat(fd, &sb);
+		assert(ret != -1);
+		length = sb.st_size;
+		//printf("length:%zu\n", length);
+
+		//printf("Memory mapping....\n");
+		p1 = mmap(NULL, length, PROT_READ | PROT_WRITE,
+			MAP_PRIVATE,
+			fd, 0);
+		assert(p1 != MAP_FAILED);
+
+		/* Trigger page fault interrupt */
+		c = p1[0];
+
+		//printf("Writing....\n");
+		//memset(p1, 0xb5, 4096);
+		//msync(p1, length, MS_SYNC);
+
+		//printf("Memory unmapping....\n");
+		ret = munmap(p1, length);
+		assert(ret != -1);
+
+	} else if (run_pread) {
+		offset = 0;
+		ret = pread(fd, buf, sizeof(buf), offset);
+		assert(ret != 1);
+
+		//printf("0x%x-0x%x\n", buf[0], buf[BUFSZ - 1]);
+	}
+
 	close(fd);
+
+	return 0;
+}
+
+void
+usage(void)
+{
+	printf("usage:\t./main [-r] [-s] [-h]\n\t");
+	printf("-r: Use pread syscall\n\t");
+	printf("-s: Use sysmap syscall\n\t");
+	printf("-h: Show help info\n");
+
+	exit(-1);
+}
+
+int
+main(int argc, char *argv[])
+{
+        int ch;
+	run_pread = false;
+	run_sysmap = false;
+
+	if (argc == 1)
+		usage();
+
+        while ((ch = getopt(argc, argv, "rsh")) != -1) {
+                switch (ch) {
+                case 'r':
+                        run_pread = true;
+                        break;
+                case 's':
+                        run_sysmap = true;
+                        break;
+		case 'h':
+			usage();
+			break;
+                default:
+                        printf("-----\n");
+			usage();
+                }
+        }
+        argc -= optind;
+        argv += optind;
+
+	if (run_pread == true && run_sysmap == true)
+		printf("pread and sysmap can not be all true\n");
+
+	read_file();
 
 	return 0;
 }
--- debug/real_helloworld/src/mmap/pagers/ioflow/rd-ioflow.d
+++ debug/real_helloworld/src/mmap/pagers/ioflow/rd-ioflow.d
@@ -11,9 +11,11 @@
 BEGIN
 {
         rootvnodep = (struct vnode **)0xffffffff83df5740;
+	unmapped_buf = *(caddr_t *)0xffffffff83df55c0;
 
         procname = "main";
-	kwflag = 0;
+	kprdflag = 0;
+	gcflag = 0;
 
         /* @[stack()] = count() */
         printf("-----IO Tracing Start-----");
@@ -21,83 +23,341 @@ BEGIN
         printf("rootvnode:%p", *rootvnodep);
 }
 
+/*Page Fault*/
+vnode_pager_getpages:entry
+/execname == procname/
+{}
+/*return*************************************************************************************/
+vnode_pager_getpages:return
+/execname == procname/
+{}
+
 /*Kernel Space*******************************************************************************/
-kern_writev:entry
+kern_pread:entry
 /execname == procname/
 {
 	trace(probename);
-	kwflag = 1;
+	kprdflag = 1;
+}
+
+getblk_core:entry
+/kprdflag/
+{
+        self->vp = args[0];
+        self->bpp = args[8];
+
+	/*****vnode*****/
+        printf("vp:%p tag:%p %s",
+                self->vp,
+                self->vp->v_tag,
+                stringof(self->vp->v_tag)
+                );
+        printf("\n\t\t\t\t\t      ");
+
+	/*****inode*****/
+	this->inode = (struct inode *)self->vp->v_data;
+	printf("inode: number:%d",
+		this->inode->i_number
+		);
+        printf("\n\t\t\t\t\t      ");
+
+	/*****bufobj*****/
+	this->bufobj = self->vp->v_bufobj;
+	printf("bufobj:vmobj:%p",
+		this->bufobj.bo_object
+		);
+        printf("\n\t\t\t\t\t      ");
+
+	this->bufv_clean = this->bufobj.bo_clean;
+	this->bufv_dirty = this->bufobj.bo_dirty;
+	printf("bufv clean:cnt:%d",
+		this->bufv_clean.bv_cnt
+		);
+        printf("\n\t\t\t\t\t      ");
+	printf("bufv dirty:cnt:%d",
+		this->bufv_dirty.bv_cnt
+		);
+        printf("\n\t\t\t\t\t      ");
+
+	/*stack();*/
+
+	gcflag = 1;
+}
+
+getnewbuf:entry
+/kprdflag/
+{}
+
+/*set vp*/
+bgetvp:entry
+/kprdflag/
+{
+
+	this->bp = args[1];
+	/*****buf*****/
+        printf("bp:%p bufobj:%p vp:%p",
+		this->bp,
+		this->bp->b_bufobj,
+		this->bp->b_vp
+                );
+}
+
+allocbuf_flags:entry
+/kprdflag/
+{
+	this->bp = args[0];
+	this->size = args[1];
+
+	printf("size:%d", this->size);
+        printf("\n\t\t\t\t\t      ");
+
+	/*****buf*****/
+        printf("bp:%p flags:%p bcount:%d",
+		this->bp,
+		this->bp->b_flags,
+		this->bp->b_bcount
+                );
+        printf("\n\t\t\t\t\t      ");
+
+if (0) {
+	printf("buf pages: pg0:%p pg1:%p pg2:%p",
+		this->bp->b_pages[0],
+		this->bp->b_pages[1],
+		this->bp->b_pages[2]
+		);
+}
+
+}
+
+vfs_vmio_extend:entry
+/kprdflag/
+{
+	this->bp = args[0];
+	this->desiredpages = args[1];
+	this->size = args[2];
+
+	printf("size:%d desiredpages:%d",
+		this->size, this->desiredpages);
+        printf("\n\t\t\t\t\t      ");
+
+	/*****buf*****/
+        printf("bp:%p flags:%p bcount:%d b_npages:%d",
+		this->bp,
+		this->bp->b_flags,
+		this->bp->b_bcount,
+		this->bp->b_npages
+                );
+        printf("\n\t\t\t\t\t      ");
+
+	printf("buf pages: pg0:%p pg1:%p pg2:%p",
+		this->bp->b_pages[0],
+		this->bp->b_pages[1],
+		this->bp->b_pages[2]
+		);
 }
 
 vm_page_grab_pages_unlocked_tracked:entry
-/kwflag/
+/kprdflag/
+{
+	this->allocflags = args[3];
+
+	printf("allocflags:%p", this->allocflags);
+}
+
+vm_page_acquire_unlocked:entry
+/kprdflag/
+{
+	this->mp = args[3];
+
+	printf("mp:%p", *this->mp);
+}
+
+vm_page_grab_pages_tracked:entry
+/kprdflag/
 {}
 
-vm_page_insert_radixdone:entry
-/kwflag/
+vm_page_alloc_after:entry
+/kprdflag && gcflag/
 {
-	this->m = args[0];
+	this->object = args[0];
+	this->pindex = args[1];
 
-	printf("pg:busy_lock:%d",
-		this->m->busy_lock
-		);
-
-	/*stack();*/
+	printf("object:%p pindex:%d",
+		this->object, this->pindex);
 }
 
-vm_page_sunbusy:entry
-/kwflag/
+uma_zalloc:entry
+/kprdflag && gcflag/
+{}
+
+vm_page_insert_after:entry
+/kprdflag && gcflag/
 {
-	self->m = args[0];
-
-	printf("pg:busy_lock:%d",
-		self->m->busy_lock
-		);
-
-	/*stack();*/
-}
-
-vm_page_trysbusy:entry
-/kwflag/
-{
-	self->m = args[0];
-
-	printf("pg:busy_lock:%d",
-		self->m->busy_lock
-		);
+	this->mpred = args[3];
+	printf("mpred:%p", this->mpred);
 }
 
 /*return*************************************************************************************/
-vm_page_trysbusy:return
-/kwflag/
-{
-	printf("pg:busy_lock:%d",
-		self->m->busy_lock
-		);
-}
-
-vm_page_sunbusy:return
-/kwflag/
-{
-	printf("pg:busy_lock:%d",
-		self->m->busy_lock
-		);
-	/*stack();*/
-}
-
-vm_page_insert_radixdone:return
-/kwflag/
+vm_page_insert_after:return
+/kprdflag && gcflag/
 {}
+
+uma_zalloc:return
+/kprdflag && gcflag/
+{}
+
+vm_page_alloc_after:return
+/kprdflag && gcflag/
+{}
+
+vm_page_grab_pages_tracked:return
+/kprdflag/
+{
+	this->ret = args[1];
+	printf("ret:%d", this->ret);
+}
+
+vm_page_acquire_unlocked:return
+/kprdflag/
+{
+	this->ret = args[1];
+
+	printf("mp:%p", *this->mp);
+        printf("\n\t\t\t\t\t      ");
+
+	printf("ret:%d", this->ret);
+}
 
 vm_page_grab_pages_unlocked_tracked:return
-/kwflag/
-{}
+/kprdflag/
+{
+}
 
-kern_writev:return
+vfs_vmio_extend:return
+/kprdflag/
+{
+	/*****buf*****/
+        printf("bp:%p flags:%p bcount:%d b_npages:%d",
+		this->bp,
+		this->bp->b_flags,
+		this->bp->b_bcount,
+		this->bp->b_npages
+                );
+        printf("\n\t\t\t\t\t      ");
+
+	printf("buf pages: pg0:%p pg1:%p pg2:%p",
+		this->bp->b_pages[0],
+		this->bp->b_pages[1],
+		this->bp->b_pages[2]
+		);
+}
+
+allocbuf_flags:return
+/kprdflag/
+{
+	printf("buf pages: pg0:%p pg1:%p pg2:%p",
+		this->bp->b_pages[0],
+		this->bp->b_pages[1],
+		this->bp->b_pages[2]
+		);
+}
+
+bgetvp:return
+/kprdflag/
+{
+	/*****buf*****/
+        printf("bp:%p bufobj:%p vp:%p",
+		this->bp,
+		this->bp->b_bufobj,
+		this->bp->b_vp
+                );
+        printf("\n\t\t\t\t\t      ");
+
+	this->bufv_clean = this->bp->b_bufobj->bo_clean;
+	this->bufv_dirty = this->bp->b_bufobj->bo_dirty;
+	printf("bufv clean:cnt:%d", this->bufv_clean.bv_cnt);
+        printf("\n\t\t\t\t\t      ");
+	printf("bufv dirty:cnt:%d", this->bufv_dirty.bv_cnt);
+        printf("\n\t\t\t\t\t      ");
+}
+
+getnewbuf:return
+/kprdflag/
+{
+	this->bp = args[1];
+	/*****buf*****/
+        printf("bp:%p bcount:%d bufsize:%d flags:%x data:%p",
+                this->bp,
+		this->bp->b_bcount,
+		this->bp->b_bufsize,
+                this->bp->b_flags,
+                this->bp->b_data
+                );
+        printf("\n\t\t\t\t\t      ");
+        printf("bp: bufobj:%p vp:%p kvabase:%p",
+		this->bp->b_bufobj,
+		this->bp->b_vp,
+		this->bp->b_kvabase
+                );
+}
+
+getblk_core:return
+/kprdflag/
+{
+        this->bp = *self->bpp;
+
+	/*****buf*****/
+        printf("bp:%p offset:%d bcount:%d bufsize:%d flags:%x data:%p",
+                this->bp,
+		this->bp->b_offset,
+		this->bp->b_bcount,
+		this->bp->b_bufsize,
+                this->bp->b_flags,
+                this->bp->b_data
+                );
+        printf("\n\t\t\t\t\t      ");
+        printf("bp: bufobj:%p vp:%p kvabase:%p",
+		this->bp->b_bufobj,
+		this->bp->b_vp,
+		this->bp->b_kvabase
+                );
+        printf("\n\t\t\t\t\t      ");
+
+	printf("buf pages: pg0:%p pg1:%p pg2:%p",
+		this->bp->b_pages[0],
+		this->bp->b_pages[1],
+		this->bp->b_pages[2]
+		);
+        printf("\n\t\t\t\t\t      ");
+
+	/*****bufobj*****/
+	this->bufobj = self->vp->v_bufobj;
+	printf("bufobj:bsize:%d",
+		this->bufobj.bo_bsize
+		);
+        printf("\n\t\t\t\t\t      ");
+
+	this->bufv_clean = this->bufobj.bo_clean;
+	this->bufv_dirty = this->bufobj.bo_dirty;
+	printf("bufv clean:cnt:%d",
+		this->bufv_clean.bv_cnt);
+        printf("\n\t\t\t\t\t      ");
+	printf("bufv dirty:cnt:%d",
+		this->bufv_dirty.bv_cnt);
+        printf("\n\t\t\t\t\t      ");
+
+        printf("unmapped_buf:%p",
+                unmapped_buf
+                );
+
+	gcflag = 0;
+}
+
+kern_pread:return
 /execname == procname/
 {
 	trace(probename);
-	kwflag = 0;
+	kprdflag = 0;
 }
 
 /********************************************************************************************/
