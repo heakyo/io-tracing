5d9a17f694446fb8b8dea04b4b53842faaad526b
Marvin Ma <marvin.ma@dell.com>
2026-03-27 12:48:23 +0000

    IOFLOW: show FSID
    Signed-off-by: Marvin Ma <marvin.ma@dell.com>

--- debug/real_helloworld/src/mmap/pagers/ioflow/umount-ioflow/umount-ioflow.d
+++ debug/real_helloworld/src/mmap/pagers/ioflow/umount-ioflow/umount-ioflow.d
@@ -11,6 +11,8 @@ BEGIN
 {
         procname = "umount";
 
+	sysunmount_flag = 0;
+
         /* @[stack()] = count() */
         printf("-----IO Tracing Start-----");
 }
@@ -18,12 +20,77 @@ BEGIN
 /*Kernel Space*******************************************************************************/
 sys_unmount:entry
 /execname == procname/
-{}
+{
+	this->uap = args[1];
+
+	printf("uap:flags:%p path:%s",
+		this->uap->flags,
+		copyinstr((uintptr_t)this->uap->path)
+	);
+
+	sysunmount_flag = 1;
+}
+
+ffs_unmount:entry
+/sysunmount_flag/
+{
+	this->mp = args[0];
+
+	this->ump = (struct ufsmount *)this->mp->mnt_data;
+
+	printf("ump:fs:0x%p",
+		this->ump->um_fs
+	);
+        printf("\n\t\t\t\t\t      ");
+
+	this->fs = this->ump->um_fs;
+	printf("fs:flags:%p",
+		this->fs->fs_flags
+	);
+}
+
+ffs_sbupdate:entry
+/sysunmount_flag/
+{
+	this->ump = args[0];
+
+	this->fs = this->ump->um_fs;
+	printf("fs:flags:%p",
+		this->fs->fs_flags
+	);
+}
+
+vfs_freeopts:entry
+/sysunmount_flag/
+{
+	this->opts = args[0];
+
+	this->opt = this->opts->tqh_first;
+	printf("opt:%p name:%s",
+		this->opt,
+		stringof(this->opt->name)
+	);
+}
 
 /*return*************************************************************************************/
+vfs_freeopts:return
+/sysunmount_flag/
+{}
+
+ffs_sbupdate:return
+/sysunmount_flag/
+{}
+
+ffs_unmount:return
+/sysunmount_flag/
+{}
+
 sys_unmount:return
 /execname == procname/
-{}
+{
+	trace(probename);
+	sysunmount_flag = 0;
+}
 
 /********************************************************************************************/
 END
