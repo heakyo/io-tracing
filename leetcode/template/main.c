c621fcd270e529a183211058ee467bafd3850f75
Marvin Ma <marvin.ma@dell.com>
2026-04-05 07:26:54 +0100

    leetcode: update template
    Signed-off-by: Marvin Ma <marvin.ma@dell.com>

--- leetcode/template/main.c
+++ leetcode/template/main.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define ARRAYSIZE(a) (sizeof (a) / sizeof *(a))
+
+int quick_sort(int *array, int low, int high)
+{
+	return 0;
+}
 
 int main(int argc, char *argv[])
 {
-	printf("This is a template.\n");
+	int array[5] = {1, 7, 3, 8, 4};
+	int array_len = ARRAYSIZE(array);
+	int i;
+
+	/* Input */
+	printf("Input: [");
+	for (i = 0; i < array_len - 1; i++)
+		printf("%d ", array[i]);
+	printf("%d]\n", array[i]);
+
+	/* Algorithm */
+	quick_sort(array, 0, array_len - 1);
+
+	/* Output */
+	printf("Output: [");
+	for (i = 0; i < array_len - 1; i++)
+		printf("%d ", array[i]);
+	printf("%d]\n", array[i]);
+
 	return 0;
 }
